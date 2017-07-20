b87deb2804a363543e2d506db85e062de7221ab4
Hendrik Tekayadi <hendrik@popsical.com>
2017-07-20 14:00:54 +0800

    Event components

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'dashboard#index'
   namespace :api do
-    resources :events
+    resources :events, only: [:index, :create, :update, :destroy] do
+      get :search, on: :collection
+    end
   end
 end
